18afab20eff55cba41034a4b91b17739aae0d3bd
jesusandres31 <jesusandreszini14@gmail.com>
2022-11-04 14:56:01 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Skeleton-Deployment
+
+work in progress
+
+### keys
+
+- `someapp`: the name of the application.
+- `someuser`: the name of the user.
---
+++ env.staging/docker-push.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+#
+REPO_NAME=someapp
+USERNAME=someuser
+BRANCH=master
+
+#
+BACK_IMG_NAME=someapp-backend
+BACK_IMG_PATH=registry.gitlab.com/${USERNAME}/someapp/backend:latest
+
+FRONT_IMG_NAME=someapp-frontend
+FRONT_IMG_PATH=registry.gitlab.com/${USERNAME}/someapp/frontend:latest
+
+#
+# functins
+#
+
+# git pull
+girPull(){
+    git checkout ${BRANCH}
+
+    git pull 
+
+    git branch
+}
+
+#
+# start...
+#
+
+#
+# back
+#
+cd ../../../someapp-backend
+
+# git pull
+girPull
+
+# docker image prune -af
+docker build -t="${BACK_IMG_NAME}" .
+
+# get just created image ID
+BACK_IMG_ID=$(docker images --format "{{.ID}} {{.Repository}}" | grep ${BACK_IMG_NAME} | awk '{ print $1 }')
+
+docker tag ${BACK_IMG_ID} ${BACK_IMG_PATH}
+
+docker push ${BACK_IMG_PATH}
+
+#
+# front
+#
+cd ../someapp-frontend
+
+# git pull
+girPull
+
+# docker image prune -af
+docker build -t="${FRONT_IMG_NAME}" .
+
+# get just created image ID
+FRONT_IMG_ID=$(docker images --format "{{.ID}} {{.Repository}}" | grep ${FRONT_IMG_NAME} | awk '{ print $1 }')
+
+docker tag ${FRONT_IMG_ID} ${FRONT_IMG_PATH}
+
+docker push ${FRONT_IMG_PATH}
+
+# end...
